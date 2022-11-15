56d497d523174463f47707cb87365ef7c4d63a56
Tom Draper <tomjdraper1@gmail.com>
2022-11-15 13:32:42 +0000

    Json parsing

--- main.py
+++ main.py
@@ -3,12 +3,44 @@ import requests
 import os
 import re
 from dotenv import load_dotenv
+import ast
+import json
 
 load_dotenv()
 
 def code_snippets(readme: str) -> list[str]:
-    matches = re.findall('```[^`]```', readme)
-    print('snippets', matches)
+    matches = re.findall(r'```[^`]*?```', readme)
+    return matches
+
+def valid_python(code: str) -> bool:
+   try:
+       ast.parse(code)
+   except SyntaxError:
+       return False
+   return True
+
+def valid_json(code: str) -> bool:
+    try:
+        json.loads(code)
+    except ValueError:
+        return False
+    return True
+
+def check_snippets_lang(snippets: list[str], language_regex: str, valid: callable):
+    for snippet in snippets:
+        print('---------------- snippet')
+        print(snippet)
+        if re.match(f'```{language_regex}', snippet, re.IGNORECASE):
+            code = re.sub(f'```{language_regex}|```', '', snippet, re.IGNORECASE)
+            print('-------------------------------------------------------------------------')
+            print(code)
+            print('-------------------------------------------------------------------------')
+            x = valid(code)
+            print(x)
+
+def check_snippets(snippets: list[str]):
+    check_snippets_lang(snippets, 'py(thon)?', valid_python)
+    check_snippets_lang(snippets, 'json', valid_json)
 
 TOKEN = os.getenv('TOKEN')
 headers = {
@@ -18,18 +50,20 @@ headers = {
 url = "https://api.github.com/search/code?q=```filename:README.md+language:markdown"
 response = requests.request("GET", url, headers=headers)
 
-d = response.json()
+if response.status_code == 200:
+    d = response.json()
 
-pprint(d)
+    pprint(d)
 
-for result in d['items']:
-    readme_url = result['html_url']
-    print(readme_url)
-    raw_readme_url = readme_url.replace('https://github.com', 'https://raw.githubusercontent.com').replace('blob/', '')
-    print(raw_readme_url)
-    
-    response = requests.get(raw_readme_url)
-    if response.status_code == 200:
-        readme = response.text
-        print(readme)
-        print(code_snippets(readme))
+    for result in d['items']:
+        readme_url = result['html_url']
+        raw_readme_url = readme_url.replace('https://github.com', 'https://raw.githubusercontent.com').replace('blob/', '')
+        
+        response = requests.get(raw_readme_url)
+        if response.status_code == 200:
+            readme = response.text
+            snippets = code_snippets(readme)
+            check_snippets(snippets)
+
+else:
+    print(f'error: status code: {response.status_code}')
